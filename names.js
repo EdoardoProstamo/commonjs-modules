a014ea17ec5f428358f215cc53b1cd0094490030
EdoardoProstamo <edoardoprostamo.19@libero.it>
2025-03-26 00:56:34 +0100

    feat: funzione  names.js

--- names.js
+++ names.js
@@ -1,2 +1,14 @@
 // 1. Crea una funzione che accetta due parametri: firstName, lastName. La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
 // 2. Esporta la funzione dal file.
+
+
+// arrow function
+// const allName = (firstName, lastName) => {firstName, lastName};
+
+
+function allName(firstName, lastName) {
+    return {
+        firstName,
+        lastName
+    }
+};
