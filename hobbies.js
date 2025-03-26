bcc14a51b30c5443aa1f6ec838cbe8d5c704be69
EdoardoProstamo <edoardoprostamo.19@libero.it>
2025-03-26 01:33:44 +0100

    feat: funzione people.js

--- hobbies.js
+++ hobbies.js
@@ -1,8 +1,6 @@
 // 1. Crea una funzione che accetta tre parametri: hobbyOne, hobbyTwo, hobbyThree. La funzione dovrebbe restituire  un array di stringhe contenenti gli hobby.
 // 2. Esporta la funzione dal file.
 
-// arrow function
-// const allHobbyes = (hobbyOne, hobbyTwo, hobbyThree) => {hobbyOne, hobbyTwo, hobbyThree};
 
 function allHobbyes(hobbyOne, hobbyTwo, hobbyThree) {
     return [hobbyOne, hobbyTwo, hobbyThree];
--- people.js
+++ people.js
@@ -1,3 +1,13 @@
 // 1. Importa la tua funzione da names.js
 // 2. Importa la tua funzione da hobbies.js
 // 3. Crea una funzione che non ha parametri. La funzione dovrebbe restituire un oggetto con due proprietà: fullName ed hobbies. All'interno della tua funzione, usa le tue due funzioni precedenti per costruire l'oggetto.
+
+function allPeople() {
+    return {
+        fullName: allName ('nome', 'cognome'),
+        hobbies: allHobbyes ('a', 'b', 'c')
+    };
+};
+
+// invoco la funzione
+console.log(allPeople());
