a8f2f1ad7a6243c65345ded899bfe848efb01dfe
EdoardoProstamo <edoardoprostamo.19@libero.it>
2025-03-26 01:49:00 +0100

    feat: importo i moduli precedenti in people.js

--- people.js
+++ people.js
@@ -2,6 +2,12 @@
 // 2. Importa la tua funzione da hobbies.js
 // 3. Crea una funzione che non ha parametri. La funzione dovrebbe restituire un oggetto con due proprietà: fullName ed hobbies. All'interno della tua funzione, usa le tue due funzioni precedenti per costruire l'oggetto.
 
+const allName = require('./names');
+const allHobbyes = require('./hobbies');
+
+
+
+
 
 function allPeople() {
     return {
